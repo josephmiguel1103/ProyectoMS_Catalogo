50d51e3220d05ee7bd2a93e3764be188aef87068
Your Namejosephmiguel1103 <josephmiguel1103@gmail.com>
2024-09-02 09:03:53 -0500

    gateway

---
+++ programa9/src/main/java/com/example/programa9/Programa9Application.java
@@ -0,0 +1,13 @@
+package com.example.programa9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programa9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Programa9Application.class, args);
+    }
+
+}
---
+++ programa9/src/test/java/com/example/programa9/Programa9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programa9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programa9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
