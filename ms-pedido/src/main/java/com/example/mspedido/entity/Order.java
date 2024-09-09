a11b86fd078cce5f11578c51ea81164484f0abf8
Your Namejosephmiguel1103 <josephmiguel1103@gmail.com>
2024-09-09 09:54:07 -0500

    asd

--- ms-pedido/src/main/java/com/example/mspedido/entity/Order.java
+++ ms-pedido/src/main/java/com/example/mspedido/entity/Order.java
@@ -1,6 +1,5 @@
 package com.example.mspedido.entity;
 
-import com.example.mspedido.dto.ClienteDto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -9,20 +8,14 @@ import java.util.List;
 
 @Entity
 @Data
-
-public class Pedido {
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String serie;
-    private String numero;
-    private String descripcion;
-    private Integer clienteId;
+    private String number;
+    private Integer ClientId;
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "venta_id")
-    private List<PedidoDetalle> detalle;
-
-    @Transient
-    private ClienteDto clienteDto;
+    @JoinColumn(name = "order_id")
+    private List<OrderDetail> orderDetails;
 }
